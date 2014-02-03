3d52441d41ca3f701c156182323e1e2a8b32eb5f
yollo <manssond@gmail.com>
2014-02-03 12:23:44 +0100

    har en Main i dal.sc, programmet är körbart!

--- T4/T4/Dal/Dal.cs
+++ T4/T4/Dal/Dal.cs
@@ -3,10 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.SqlClient;
 
 namespace T4.Dal
 {
     class Dal
     {
+        public static void Main() //detta fungerar! Hurra!!
+        {
+            Dal d = new Dal();
+            Console.WriteLine(d.TestarSql());
+        }
+        SqlConnection sc = new SqlConnection("Data Source=DAVID-PC;Integrated Security=SSPI;Initial Catalog=T4");
+        SqlCommand cmd;
+
+        public string TestarSql()
+        {
+            sc.Open();
+            cmd = new SqlCommand("Insert into whore values('Kenny')", sc);
+            cmd.ExecuteNonQuery();
+            sc.Close();
+            return "Success";
+        }
     }
 }
